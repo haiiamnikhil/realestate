f16ce035937e7e79829f281d374d219cb2c09082
haiiamnikhil <haiiamnikhil@gmail.com>
2022-10-28 17:57:58 +0530

    User property listing changes

--- notifications/views/common/email_notifier.py
+++ notifications/views/common/email_notifier.py
@@ -2,21 +2,43 @@ from django.core.mail import send_mail, send_mass_mail, EmailMultiAlternatives
 from django.conf import settings
 from django.template.loader import render_to_string
 
+from properties.models import Properties
+from system_settings.models import EmailSettings
+
 
 class EmailNotifier:
-    
+
     def __init__(self):
         self.template_name = {
-            'enquiry' : 'common/email/prop_enquiry.html'
+            'enquiry': 'common/email/prop_enquiry.html'
         }
         self.from_email = settings.EMAIL_HOST_USER
-    
-    def send_enquiry_email(self, enquiry):
+
+    def send_email_notification(self, data: dict, user=None, type=None):
+        params = {}
+        email_settings = EmailSettings.objects.get(user__user=user)
+        if email_settings.is_enabled:
+            params['subject'] = Properties.objects.get(
+                uid=data['prop_uid']).heading
+            params['receiver'] = email_settings.email
+            params['type'] = type
+            params['body'] = {
+                'comment': data['comment'],
+                'phone': data['phone'],
+                'name': data['full_name'],
+                'email': data['email']
+            }
+            self.send_email(params)
+        else:
+            pass
+
+    def send_email(self, enquiry):
 
         subject = enquiry['subject']
         recipient = [enquiry['receiver']]
         template = self.template_name[enquiry['type']]
         html_content = render_to_string(template, enquiry)
-        email = EmailMultiAlternatives(subject=subject,from_email=self.from_email,to=recipient)
-        email.attach_alternative(html_content,'text/html')
+        email = EmailMultiAlternatives(
+            subject=subject, from_email=self.from_email, to=recipient)
+        email.attach_alternative(html_content, 'text/html')
         email.send()
--- properties/ajax/property.py
+++ properties/ajax/property.py
@@ -2,6 +2,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.core import serializers
 
 from properties.models import Properties
 from utils.common.response.response import ResponseGenerator
@@ -21,6 +22,19 @@ def delete_prop(request):
             response = response_render.render_response_json(True, message='Property deleted')
             return JsonResponse(response, safe=False)
 
+        except Exception as e:
+            print(e)
+            response = response_render.render_response_json(False, message='Something went wrong')
+            return JsonResponse(response, safe=False)
+
+
+def get_latest_properties(request):
+    if request.method == 'GET':
+        try:
+            properties = Properties.objects.filter(status='active').order_by('-created_at')
+            data = serializers.serialize('json',properties)
+            response = response_render.render_response_json(True, data=data)
+            return JsonResponse(response, safe=False)
         except Exception as e:
             print(e)
             response = response_render.render_response_json(False, message='Something went wrong')
